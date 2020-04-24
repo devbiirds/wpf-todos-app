0807a611848d9acc9f56b34755b32e24c1b3fe96
vadim <vadikoff57@gmail.com>
2020-04-24 12:32:27 +0300

    feat:add new features in 9 labs

--- Wpf_7_8/MainWindow.xaml.cs
+++ Wpf_7_8/MainWindow.xaml.cs
@@ -63,6 +63,10 @@ namespace Wpf_7_8
           
             Category.ItemsSource = CategoryL;
             Priority.ItemsSource = PriorityL;
+            List<string> styles = new List<string> { "light", "dark" };
+            styleBox.SelectionChanged += ThemeChange;
+            styleBox.ItemsSource = styles;
+            styleBox.SelectedItem = "dark";
         }
 
         public void ShowViewModel()
@@ -231,6 +235,21 @@ namespace Wpf_7_8
                 MessageBox.Show("Успешно удалено!");
             }
         }
-        
+        private void customControl_Click(object sender, RoutedEventArgs e)
+        {
+            txtBlock.Text = "WPF App by Vadim Minakov";
+        }
+        private void ThemeChange(object sender, SelectionChangedEventArgs e)
+        {
+            string style = styleBox.SelectedItem as string;
+            // определяем путь к файлу ресурсов
+            var uri = new Uri("Themes/" +style + ".xaml", UriKind.Relative);
+            // загружаем словарь ресурсов
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            // очищаем коллекцию ресурсов приложения
+            Application.Current.Resources.Clear();
+            // добавляем загруженный словарь ресурсов
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+        }
     }
 }
